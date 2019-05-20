bda63984522af343d14beeab3d0b43dda001b371
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-05-20 10:44:28 -0400

    more cleanup

--- jeremy_z_portfolio/src/components/ParallaxDiv2.jsx
+++ jeremy_z_portfolio/src/components/ParallaxDiv2.jsx
@@ -18,7 +18,7 @@ export default class ParallaxDiv2 extends Component {
         <img id='skillsa' src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/noun_Html%20file%20format_374578.png?raw=true' alt='spritehtml' />
         <img id='skillsa' src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/noun_CSS%20File_237094.png?raw=true' alt='spritecss' />
         <img id='skillsa' src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/kisspng-ruby-on-rails-the-ruby-programming-language-comput-rirug-w-john-sawers-your-emotional-api-mojotech-5b651b19434131.6405522415333527292755.png?raw=true' alt='spriteruby' />
-        <img className='bio-photo' src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/webdev_profileimg.jpg?raw=true' alt='jeremyz' alt='spritegit' />
+        <img className='bio-photo' src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/webdev_profileimg.jpg?raw=true' alt='jeremyz' />
         <img id='skillsb' src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/githublogo.png?raw=true' alt='spritejs' />
         <img id='skillsb' src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/kisspng-javascript-node-js-computer-icons-logo-application-prog-nodejs-svg-png-icon-free-download-437-44-5ba3cfbd27d1b0.8756216715374622051631.png?raw=true' alt='spritereact' />
         <img id='skillsb' src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/kisspng-react-javascript-library-web-development-vue-js-funding-icon-5b51604fee6bc6.4919910515320597279766.png?raw=true' alt='sprites' />
--- jeremy_z_portfolio/src/components/PortfolioProjects.jsx
+++ jeremy_z_portfolio/src/components/PortfolioProjects.jsx
@@ -4,15 +4,15 @@ import './PortfolioProjects.css'
 export default class PortfolioProjects extends Component {
   render() {
     return (
-      <div class="projects">
+      <div className="projects">
         <form id="contact" action="" method="post">
           <h3>Contact Me</h3>
           <br />
           <fieldset>
-            <input placeholder="Your Email Address" type="email" tabindex="2" required />
+            <input placeholder="Your Email Address" type="email" tabIndex="2" required />
           </fieldset>
           <fieldset>
-            <textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
+            <textarea placeholder="Type your Message Here...." tabIndex="5" required></textarea>
           </fieldset>
           <fieldset>
             <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
