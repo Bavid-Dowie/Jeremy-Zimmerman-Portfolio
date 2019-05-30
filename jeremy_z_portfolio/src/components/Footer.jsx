ec563550803c5a464acf586cbe0bdff8b2182807
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-05-30 12:30:52 -0400

    color scheme updated

--- jeremy_z_portfolio/src/components/Footer.jsx
+++ jeremy_z_portfolio/src/components/Footer.jsx
@@ -13,9 +13,6 @@ export default class Footer extends Component {
               <li><a href='https://github.com/Bavid-Dowie.com/jeremyzimmermanmusic/'>GitHub-</a></li>
             </ul>
           </div>
-          <div className='copyright'>
-            <p>Copyright 2019</p>
-          </div>
         </footer>
       </div>
     )
--- jeremy_z_portfolio/src/components/PortfolioMain.jsx
+++ jeremy_z_portfolio/src/components/PortfolioMain.jsx
@@ -26,6 +26,9 @@ class PortfolioMain extends Component {
         <div className='projects'>
           <div id='col' className='project4'>
             <a href='http://webplayer-lite.surge.sh/main'><img src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/Screen%20Shot%202019-05-20%20at%202.44.02%20AM.png?raw=true' alt='webplayer' /></a>
+          <p className='webPlayerText'>
+            webPlayerLite is an audio-media aggregator, which accesses the SoundCloud API to search for audio files by any keyword in order to populate the audio players on the app's main page. <br />
+          </p>
           </div>
           <div id='col' className='project1'>
             <a href='http://uxwdi-sushizo.surge.sh/'><img src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/Screen%20Shot%202019-05-20%20at%202.04.37%20AM.png?raw=true' alt='Sushi Zo' />
