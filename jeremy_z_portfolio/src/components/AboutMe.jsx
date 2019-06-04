cb5f3e7d95d1ee8868d37773e67a1abbe7a5ee7c
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-06-04 12:32:01 -0400

    about me blurb added

--- jeremy_z_portfolio/src/components/AboutMe.jsx
+++ jeremy_z_portfolio/src/components/AboutMe.jsx
@@ -7,6 +7,14 @@ export default class AboutMe extends Component {
       <div
         id='aboutme_jump'
         className='about-me'>
+        <h1 className='about-me-blurb'>As a full-stack developer and musician currently based in NYC, I thrive and excel in creative environments.
+        <br />
+        <br />
+        As a lifelong traveler, endlessly wandering across longitudes and latitudes, I have a deep-seeded desire to help bring people together.
+        <br />
+        <br />
+        As a dreamer I believe the future exists for everyone. Through creativity, care, and collaboration, the future belongs to us all.
+        </h1>
       </div>
     )
   }
