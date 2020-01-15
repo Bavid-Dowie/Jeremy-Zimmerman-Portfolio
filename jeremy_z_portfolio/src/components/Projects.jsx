d93e5150aea433c7a39278b90c4a923fe84ff2b6
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2020-01-15 17:28:59 -0500

    project image addresses updated

--- jeremy_z_portfolio/src/components/Projects.jsx
+++ jeremy_z_portfolio/src/components/Projects.jsx
@@ -19,49 +19,75 @@ document.addEventListener('DOMContentLoaded', function () {
 class Projects extends Component {
   render() {
     return (
-      <div
-        className='projects__body'
-        id='projects_jump'>
+      <div className="projects__body" id="projects_jump">
         <title>Jeremy Zimmerman - A Profile</title>
         <hr />
-        <h1 className='main-header'>My Stuff</h1>
+        <h1 className="main-header">My Stuff</h1>
         <hr />
-        <div className='projects'>
-          <div
-            id='col'
-            className='project1'>
-            <a href='http://webplayer-lite.surge.sh/main'>
-              <img src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/sc-player-screenshot.png?raw=true' alt='webplayer' />
+        <div className="projects">
+          <div id="col" className="project1">
+            <a href="http://webplayer-lite.surge.sh/main">
+              <img
+                src="https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/56bc90b1d3f435bf9bd71e10cbd68e7ab5bf3ab1/sc-player-screenshot.png?raw=true"
+                alt="webplayer"
+              />
             </a>
-            <p className='appText'>
-              webPlayerLite - HTML, CSS, React.js, SoundCloud API <br /><br />An audio-media aggregator, which accesses the SoundCloud API to search for audio files by any keyword in order to populate audio players for users to be able to listen to their desired music selection.  <br />
+            <p className="appText">
+              webPlayerLite - HTML, CSS, React.js, SoundCloud API <br />
+              <br />
+              An audio-media aggregator, which accesses the SoundCloud API to
+              search for audio files by any keyword in order to populate audio
+              players for users to be able to listen to their desired music
+              selection. <br />
             </p>
           </div>
-          <div id='col' className='project2'>
-            <a href='http://uxwdi-sushizo.surge.sh/'>
-              <img src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/sushi-zo-screenshot.png?raw=true' alt='Sushi Zo' />
+          <div id="col" className="project2">
+            <a href="http://uxwdi-sushizo.surge.sh/">
+              <img
+                src="https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/56bc90b1d3f435bf9bd71e10cbd68e7ab5bf3ab1/sushi-zo-screenshot.png?raw=true"
+                alt="Sushi Zo"
+              />
             </a>
-            <p className='appText'>
-              Sushi Zo - HTML, CSS, React.js <br /><br />An upgraded website built in collaboration with a team of UX/UI designers in order to create an visually pleasing and easily navigable single page site for a local restaurant.  <br />
+            <p className="appText">
+              Sushi Zo - HTML, CSS, React.js <br />
+              <br />
+              An upgraded website built in collaboration with a team of UX/UI
+              designers in order to create an visually pleasing and easily
+              navigable single page site for a local restaurant. <br />
             </p>
           </div>
-          <div id='col' className='project3'>
-            <a href='https://iterator.surge.sh/'>
-              <img src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/iterator-screenshot.png?raw=true' alt='Iterator' /></a>
-            <p className='appText'>
-              iterator - HTML, CSS, React.js, Express, Sequelize <br /><br />A coding ideas and solutions platform focused on letting developers share ideas and solutions in order to tackle the most difficult challenges faced in today's world.  <br />
+          <div id="col" className="project3">
+            <a href="https://iterator.surge.sh/">
+              <img
+                src="https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/56bc90b1d3f435bf9bd71e10cbd68e7ab5bf3ab1/iterator-screenshot.png?raw=true"
+                alt="Iterator"
+              />
+            </a>
+            <p className="appText">
+              iterator - HTML, CSS, React.js, Express, Sequelize <br />
+              <br />A coding ideas and solutions platform focused on letting
+              developers share ideas and solutions in order to tackle the most
+              difficult challenges faced in today's world. <br />
             </p>
           </div>
-          <div id='col' className='project4'>
-            <a href='http://loopit.surge.sh/login'>
-              <img src='https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/loopit-screenshot.png?raw=true' alt='LoopIt' /></a>
-            <p className='appText4'>
-              LoopIt - HTML, CSS, React.js on Rails, Postgresql <br /><br />A fullstack application allowing users to create their own customized drumpad and loopstation. Users will ultimately be able to record their own short audio samples and create audio loops with their own custom drumpad.  <br />
+          <div id="col" className="project4">
+            <a href="http://loopit.surge.sh/login">
+              <img
+                src="https://github.com/Bavid-Dowie/Jeremy-Zimmerman-Portfolio/blob/master/loopit-screenshot.png?raw=true"
+                alt="LoopIt"
+              />
+            </a>
+            <p className="appText4">
+              LoopIt - HTML, CSS, React.js on Rails, Postgresql <br />
+              <br />A fullstack application allowing users to create their own
+              customized drumpad and loopstation. Users will ultimately be able
+              to record their own short audio samples and create audio loops
+              with their own custom drumpad. <br />
             </p>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
